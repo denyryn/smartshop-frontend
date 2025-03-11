0f3321704446242baea8c9bf9a5eecd11226de2e
Deny <denyryn@outlook.com>
2025-03-11 22:22:22 +0700

    chore: update Tailwind config with typography and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 import { ColorVariables } from "./constants/ColorVariables";
+import { TypographyVariables } from "./constants/TypographyVariables";
+
 module.exports = {
+  darkMode: "class",
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      extend: {
-        colors: {
-          ...ColorVariables,
-        },
+      colors: {
+        ...ColorVariables,
       },
+      typography: {
+        ...TypographyVariables
+      }
     },
   },
   plugins: [require("daisyui")],
