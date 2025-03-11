acdd9ea402f0cd8ad648d247f25b580a50a3f875
Deny <denyryn@outlook.com>
2025-03-11 22:22:03 +0700

    fix: minor update to not-found page (not final version)

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -9,9 +9,9 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+        <ThemedText type="h1">This screen doesn't exist.</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText >Go to home screen!</ThemedText>
         </Link>
       </ThemedView>
     </>
