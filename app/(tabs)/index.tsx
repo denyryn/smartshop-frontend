5cca25edae9d85ecac4b5b66323277e668dd5ef1
paresiqbal <thecrowdel@gmail.com>
2025-03-11 20:49:37 +0700

    feat: Add search input to HomeScreen for improved navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,8 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, TextInput, View } from "react-native";
 
-import { useState } from "react";
-import { View } from "react-native";
-import CustomTextInput from "@/components/CustomTextInput";
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+// components
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { Text, Button } from "react-native";
 
 import * as Iconsax from "iconsax-react-native";
 
@@ -23,6 +18,13 @@ export default function HomeScreen() {
           <ThemedText className="text-lg">Hi Pares!</ThemedText>
         </View>
         <Iconsax.Notification size={24} />
+      </View>
+      <View className="flex-row items-center w-full mt-8 p-2 border border-gray-300 rounded-lg">
+        <TextInput
+          placeholder="Search"
+          className="flex-1 ml-2 text-base focus:outline-none"
+        />
+        <Iconsax.SearchNormal size={20} className="text-gray-500" />
       </View>
     </ThemedView>
   );
