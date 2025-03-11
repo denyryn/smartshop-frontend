da156340d0a6ca0cd1a33aae91efdf7b9b2dfab0
paresiqbal <thecrowdel@gmail.com>
2025-03-11 20:42:19 +0700

    feat: Enhance HomeScreen with user greeting and notification icon

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,35 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform } from "react-native";
 
-import { useState } from 'react';
-import { View } from 'react-native';
-import CustomTextInput from '@/components/CustomTextInput';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Text, Button } from 'react-native';
+import { useState } from "react";
+import { View } from "react-native";
+import CustomTextInput from "@/components/CustomTextInput";
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Text, Button } from "react-native";
+
+import * as Iconsax from "iconsax-react-native";
 
 export default function HomeScreen() {
-
   return (
-    <ThemedView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <ThemedText type='h1' style={styles.title}>
-        This is Home Page
-      </ThemedText>
+    <ThemedView style={{ flex: 1, alignItems: "center" }} className="p-4">
+      <View className="flex-row items-center justify-between w-full">
+        <View className="flex-row items-center space-x-2">
+          <Image
+            source={require("../../assets/images/profile.png")}
+            className="w-8 h-8 rounded-full"
+          />
+          <ThemedText className="text-lg">Hi Pares!</ThemedText>
+        </View>
+        <Iconsax.Notification size={24} />
+      </View>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   title: {
-    alignSelf: 'center',
+    alignSelf: "center",
   },
 });
