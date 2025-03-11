aec522e4e374a71ab75d09a3b87edc436b0b7187
Deny <denyryn@outlook.com>
2025-03-11 22:22:44 +0700

    feat: add TypographyVariables constants for typography styles

---
+++ constants/TypographyVariables.ts
@@ -0,0 +1,22 @@
+export const TypographyVariables = {
+    // Displays
+    display1: "leading-[110%] spacing-[-1px] text-[56px] font-normal",
+    display2: "leading-[135%] spacing-[0px] text-[28px] font-medium",
+    display3: "leading-[120%] spacing-[0px] text-[28px] font-normal",
+
+    // Headings
+    h1: "leading-[110%] spacing-[-1px] text-[40px] font-semibold",
+    h2: "leading-[100%] spacing-[-1px] text-[32px] font-medium",
+    h3: "leading-[100%] spacing-[-0.5px] text-[30px] font-medium",
+    h4: "leading-[135%] spacing-[-1px] text-[28px] font-medium",
+    h5: "leading-[145%] spacing-[-1px] text-[22px] font-medium",
+
+    // Bodies
+    bodyXxl: "leading-[130%] spacing-[0px] text-[20px]",
+    bodyXl: "leading-[145%] spacing-[-1px] text-[18px]",
+    bodyLg: "leading-[140%] spacing-[0px] text-[16px]",
+    body: "leading-[145%] spacing-[0px] text-[14px]",
+    bodySm: "leading-[150%] spacing-[0px] text-[12px]",
+    bodyXs: "leading-[160%] spacing-[0px] text-[10px]",
+
+}
