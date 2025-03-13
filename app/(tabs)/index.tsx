c061b50ba4cacc0efe433dba95c0601b9eed4399
paresiqbal <thecrowdel@gmail.com>
2025-03-13 08:01:54 +0700

    feat: Enhance HomeScreen layout with ScrollView for better product display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, TextInput, View } from "react-native";
+import { Image, StyleSheet, TextInput, View, ScrollView } from "react-native";
 
 // components
 import { ThemedText } from "@/components/ThemedText";
@@ -7,57 +7,103 @@ import { ThemedView } from "@/components/ThemedView";
 import * as Iconsax from "iconsax-react-native";
 
 export default function HomeScreen() {
+  const productCards = [1, 2, 3]; // loop three times
+
   return (
-    <ThemedView className="p-4 flex-1">
-      <View className="flex-row items-center justify-between w-full">
-        <View className="flex-row items-center space-x-2">
-          <Image
-            source={require("../../assets/images/profile.png")}
-            className="w-8 h-8 rounded-full"
-          />
-          <ThemedText className="text-lg">Hi Pares!</ThemedText>
-        </View>
-        <Iconsax.Notification size={24} />
-      </View>
-
-      <View className="flex-row items-center w-full mt-8 p-2 border border-gray-300 rounded-lg">
-        <TextInput
-          placeholder="Search"
-          className="flex-1 ml-2 text-base focus:outline-none"
-        />
-        <Iconsax.SearchNormal size={20} className="text-gray-500" />
-      </View>
-
-      <View className="border mt-6 rounded-2xl w-3/4 overflow-hidden">
-        <View style={{ width: "100%", aspectRatio: 16 / 9 }}>
-          <Image
-            source={require("../../assets/images/product.png")}
-            style={{ width: "100%", height: "100%" }}
-            resizeMode="cover"
-            className="rounded-2xl"
-          />
-        </View>
-        <View className="p-4 gap-2">
-          <ThemedText className="text-lg font-semibold">
-            Best Earphone
-          </ThemedText>
-          <View className="flex-row justify-between items-center">
-            <ThemedText className="text-sm font-medium">Toko Gadget</ThemedText>
-            <ThemedText className="text-sm font-medium">Rp. 500.000</ThemedText>
+    <ThemedView className="flex-1 pt-6">
+      <ScrollView contentContainerStyle={{ padding: 16 }}>
+        <View className="flex-row items-center justify-between mt-8 w-full">
+          <View className="flex-row items-center space-x-2">
+            <Image
+              source={require("../../assets/images/profile.png")}
+              className="w-8 h-8 rounded-full"
+            />
+            <ThemedText className="text-lg text-[black]">Hi Pares!</ThemedText>
           </View>
-          <View className="flex-row justify-start items-start">
-            <View className="flex-row items-center gap-2">
-              <Image
-                source={require("../../assets/icons/star.svg")}
-                style={{ height: 16, width: 16 }}
-              />
-              <ThemedText className="text-xs font-medium">
-                4.3 | 155 Rating | 99 Reviews
+          <Iconsax.Notification size={24} />
+        </View>
+
+        <View className="flex-row items-center w-full mt-8 p-2 border border-gray-100 rounded-lg">
+          <TextInput
+            placeholder="Search"
+            className="flex-1 ml-2 text-base focus:outline-none"
+          />
+          <Iconsax.SearchNormal size={20} className="text-gray-500" />
+        </View>
+
+        <View className="border border-gray-100 mt-6 rounded-2xl w-3/4 overflow-hidden">
+          <View style={{ width: "100%", aspectRatio: 16 / 9 }}>
+            <Image
+              source={require("../../assets/images/product.png")}
+              style={{ width: "100%", height: "100%" }}
+              resizeMode="cover"
+              className="rounded-2xl"
+            />
+          </View>
+          <View className="p-4 gap-2">
+            <ThemedText className="text-lg font-semibold">
+              Best Earphone
+            </ThemedText>
+            <View className="flex-row justify-between items-center">
+              <ThemedText className="text-sm font-medium">
+                Toko Gadget
               </ThemedText>
+              <ThemedText className="text-sm font-medium">
+                Rp. 500.000
+              </ThemedText>
+            </View>
+            <View className="flex-row justify-start items-start">
+              <View className="flex-row items-center gap-2">
+                <Image
+                  source={require("../../assets/icons/star.svg")}
+                  style={{ height: 12, width: 12 }}
+                />
+                <ThemedText className="text-xs font-medium">
+                  4.3 | 155 Rating | 99 Reviews
+                </ThemedText>
+              </View>
             </View>
           </View>
         </View>
-      </View>
+
+        {productCards.map((_, index) => (
+          <View
+            key={index}
+            className="flex flex-row items-center mt-4 border border-gray-100 rounded-2xl"
+          >
+            <View>
+              <Image
+                source={require("../../assets/images/product2.png")}
+                className="rounded-l-2xl"
+              />
+            </View>
+            <View className="p-4 gap-2 flex-grow">
+              <ThemedText className="text-lg font-semibold">
+                Best Earphone
+              </ThemedText>
+              <View className="flex-row justify-between items-center">
+                <ThemedText className="text-sm font-medium">
+                  Toko Gadget
+                </ThemedText>
+                <ThemedText className="text-sm font-medium">
+                  Rp. 500.000
+                </ThemedText>
+              </View>
+              <View className="flex-row justify-start items-start">
+                <View className="flex-row items-center gap-2">
+                  <Image
+                    source={require("../../assets/icons/star.svg")}
+                    style={{ height: 12, width: 12 }}
+                  />
+                  <ThemedText className="text-xs font-medium">
+                    4.3 | 155 Rating | 99 Reviews
+                  </ThemedText>
+                </View>
+              </View>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </ThemedView>
   );
 }
