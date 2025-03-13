ceb21d089684fcbe6d6c7da45587a08e6caac580
paresiqbal <thecrowdel@gmail.com>
2025-03-13 07:37:48 +0700

    feat: Refactor HomeScreen product display for improved layout and styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -29,45 +29,32 @@ export default function HomeScreen() {
       </View>
 
       <View className="border mt-6 rounded-2xl w-3/4 overflow-hidden">
-        <Image
-          source={require("../../assets/images/product.png")}
-          className="w-full h-48 object-contain"
-        />
+        <View style={{ width: "100%", aspectRatio: 16 / 9 }}>
+          <Image
+            source={require("../../assets/images/product.png")}
+            style={{ width: "100%", height: "100%" }}
+            resizeMode="cover"
+            className="rounded-2xl"
+          />
+        </View>
         <View className="p-4 gap-2">
-          <ThemedText className="text-lg">Best Earphone</ThemedText>
+          <ThemedText className="text-lg font-semibold">
+            Best Earphone
+          </ThemedText>
           <View className="flex-row justify-between items-center">
-            <ThemedText className="text-sm">Toko Gadget</ThemedText>
-            <ThemedText className="text-sm">Rp. 500.000</ThemedText>
+            <ThemedText className="text-sm font-medium">Toko Gadget</ThemedText>
+            <ThemedText className="text-sm font-medium">Rp. 500.000</ThemedText>
           </View>
           <View className="flex-row justify-start items-start">
             <View className="flex-row items-center gap-2">
               <Image
                 source={require("../../assets/icons/star.svg")}
-                className="h-4 w-4"
+                style={{ height: 16, width: 16 }}
               />
-              <ThemedText className="text-xs">
+              <ThemedText className="text-xs font-medium">
                 4.3 | 155 Rating | 99 Reviews
               </ThemedText>
             </View>
-          </View>
-        </View>
-      </View>
-
-      <View className="flex-row border mt-6 rounded-2xl w-64">
-        <Image
-          source={require("../../assets/images/product2.png")}
-          className="w-4 h-4"
-        />
-        <View className="p-4 gap-2">
-          <ThemedText className="text-lg">Best Earphone</ThemedText>
-          <View className="flex-row justify-between items-center">
-            <ThemedText className="text-sm">Toko Gadget</ThemedText>
-            <ThemedText className="text-sm">Rp. 500.000</ThemedText>
-          </View>
-          <View className="flex-row justify-start items-start">
-            <ThemedText className="text-sm">
-              ⭐ 4.3 | 155 Rating | 99 Reviews
-            </ThemedText>
           </View>
         </View>
       </View>
